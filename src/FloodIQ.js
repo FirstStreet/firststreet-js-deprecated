4508053b66ae1379c9773a1c4d8a747b9120cc78
gabrielzzarate <gabrielzzarate@gmail.com>
2019-06-25 14:53:31 -0400

    eslint, jest, nodemon setup

---
+++ src/FloodIQ.js
@@ -0,0 +1,17 @@
+/**
+ * Create a FloodIQ class
+ * @constructor
+ * @param {string***REMOVED*** apiKey - A string with the base URL for account.
+ * @param {Object***REMOVED*** options - A configuration object.
+*/
+
+class FloodIQ {
+  constructor(apiKey = null, options = {***REMOVED***) {
+    if (!apiKey) {
+      throw new Error('Missing API Key.');
+    ***REMOVED***
+
+  ***REMOVED***
+***REMOVED***
+
+module.exports = FloodIQ;

6998a744f55b605ee8da21eadb65f4868e634796
gabrielzzarate <gabrielzzarate@gmail.com>
2019-06-25 14:53:31 -0400

    eslint, jest, nodemon setup

---
+++ src/sum.js
@@ -0,0 +1,12 @@
+/**
+ * A function that sums two numbers.
+ * @param {number***REMOVED*** a - The first number to be added
+ * @param {number***REMOVED*** b - The second number to be added
+ * @return {number***REMOVED*** - The result
+*/
+
+function sum(a, b) {
+  return a + b;
+***REMOVED***
+
+module.exports = sum;

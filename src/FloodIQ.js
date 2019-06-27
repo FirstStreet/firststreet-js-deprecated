3a0a6adb4ed034a29659fc8bc222b43d16f46b37
gabrielzzarate <gabrielzzarate@gmail.com>
2019-06-27 16:38:17 -0400

    remove files

--- src/FloodIQ.js
+++ src/FloodIQ.js
@@ -1,15 +1,20 @@
+const Api = require('./api');
+const Http = require('./Http');
+
 /**
  * Create a FloodIQ class
  * @constructor
  * @param {string***REMOVED*** apiKey - A string with the base URL for account.
  * @param {Object***REMOVED*** options - A configuration object.
 */
-
 class FloodIQ {
   constructor(apiKey = null, options = {***REMOVED***) {
     if (!apiKey) {
       throw new Error('Missing API Key.');
     ***REMOVED***
+
+    const api = new Api(new Http(apiKey, options));
+    api.bindTo(this);
   ***REMOVED***
 ***REMOVED***
 

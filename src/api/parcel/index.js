31a1a1f73123b2574c215e7c6b4d02fc1bdd25fb
gabrielzzarate <gabrielzzarate@gmail.com>
2019-06-28 14:10:05 -0400

    start of property model

--- src/api/parcel/index.js
+++ src/api/parcel/index.js
@@ -1,28 +1,9 @@
 const { normalizeError } = require('../../Error');
 
 /**
- * A Property
- * @typedef {Object} ParcelProperty
- * @property {number} ID - The parcel unique identifier
- * @property {string} primaryNumber - The street number
- * @property {string} streetName - The street number
- * @property {string} predirection - an address element placed before the street
- * name that indicates its the geographic location
- * @property {string} postdirection - an address element placed after the street
- * name as a trailing designator such as ST for street or RD for road
- * @property {number} zipCode - The property zip code
- * @property {ParcelCityForProperty} city - The property city
- * @property {string} state - The property state
- * @property {ParcelGeometry} geometry - The viewport and bounding box of a location
- * @property {number} elevation - The elevation of a property
- * @property {number} lotSize - The size of the property
- * @property {number} floorArea - The size of the home
- * @property {string} landUse - The lot land use designator
- * @property {number} countyFips - The county federal information processing standards code
- * @property {number} distance - The distance
- */
+ * @typedef {import('../models/property').default} ParcelProperty
+*/
 
-// Todo: what is distance?, what is the units for elevation, lotSize, floorArea?
 
 /**
  * A City property on the parcel

4fa1e21b3a1f6ae0b99f1fbcdd800928913135ca
gabrielzzarate <gabrielzzarate@gmail.com>
2019-06-28 14:10:05 -0400

    start of property model

---
+++ src/models/index.js
@@ -0,0 +1,32 @@
+// readonly
+
+class BaseModel {
+  constructor(data) {
+    this.data = data;
+    this.relationships = null;
+  }
+
+  set id(id) {
+    this.data.id = id;
+    return this;
+  }
+
+  get type() {
+    return this.data.type;
+  }
+
+  get id() {
+    return this.data.id;
+  }
+
+  get raw() {
+    return this.data;
+  }
+
+  extend(key, properties) {
+    this[key] = properties;
+    return this;
+  }
+}
+
+module.exports = BaseModel;

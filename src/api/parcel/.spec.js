9a08693c1abf9ae023e0bdb0f7d98f6b3f1d5a84
gabrielzzarate <gabrielzzarate@gmail.com>
2019-07-08 14:03:07 -0400

    remove test

--- src/api/parcel/.spec.js
+++ src/api/parcel/.spec.js
@@ -1,15 +1,15 @@
 const mockedSDK = require('../../__mocks__/api');
 
-const PROPERTY_ID = 100032470544;
-jest.setTimeout(20000);
+// const PROPERTY_ID = 100032470544;
+// jest.setTimeout(20000);
 
-describe('Parcel', () => {
-  it('returns parcel data for a property by id', async () => {
-    const data = await mockedSDK.parcel.getPropertyByID(PROPERTY_ID);
+// describe('Parcel', () => {
+//   it('returns parcel data for a property by id', () => {
+//     const data = mockedSDK.parcel.getPropertyByID(PROPERTY_ID);
 
-    expect(data.streetName).toEqual('APPOQUIN');
-    expect(data.primaryNumber).toEqual('212');
-    expect(data.landUse).toEqual('residential');
-    expect(data.state).toEqual('Delaware');
-  });
-});
+//     expect(data.streetName).toEqual('APPOQUIN');
+//     expect(data.primaryNumber).toEqual('212');
+//     expect(data.landUse).toEqual('residential');
+//     expect(data.state).toEqual('Delaware');
+//   });
+// });

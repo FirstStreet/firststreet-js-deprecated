a5c730851b07d26f8ca3cf87f53fc5455cdc38d2
gabrielzzarate <gabrielzzarate@gmail.com>
2019-07-09 15:59:38 -0400

    requested changes and cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+    filename: 'floodiq.js',
     publicPath: '/',
   },
   target: 'node',

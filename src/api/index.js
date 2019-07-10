d0535d6b6f94020603052d55246ff3e906d6f7c2
gabrielzzarate <gabrielzzarate@gmail.com>
2019-07-10 16:34:53 -0400

    more lowercasing

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 const pkg = require('../../package.json');
-const parcel = require('./parcel');
+const parcel = require('./parcel/index.js');
 
 /**
  * api wrapper
--- src/api/parcel/index.js
+++ src/api/parcel/index.js
@@ -1,6 +1,6 @@
-const { normalizeError ***REMOVED*** = require('../../Error');
-const Property = require('../../models/property');
-const City = require('../../models/city');
+const { normalizeError ***REMOVED*** = require('../../Error/index.js');
+const Property = require('../../models/property/index.js');
+const City = require('../../models/city/index.js');
 
 /**
  * @typedef {import('../models/Property').default***REMOVED*** ParcelProperty

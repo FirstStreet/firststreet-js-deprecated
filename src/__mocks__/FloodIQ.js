7064847bcb3cae1ef033e40996f96988a42ad4dc
gabrielzzarate <gabrielzzarate@gmail.com>
2019-07-10 15:32:16 -0400

    add async tests, mocks, and getParcelByLatLon

---
+++ src/__mocks__/FloodIQ.js
@@ -0,0 +1,15 @@
+const Api = require('../Api');
+const Http = require('./Http');
+
+class FloodIQ {
+  constructor(apiKey = null, options = {}) {
+    if (!apiKey) {
+      throw new Error('Missing API Key.');
+    }
+
+    const api = new Api(new Http(apiKey, options));
+    api.bindTo(this);
+  }
+}
+
+module.exports = FloodIQ;

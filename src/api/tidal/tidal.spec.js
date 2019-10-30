ecf4eff6836e2d8d2749f99e3612d2d219cebe7a
gabrielzzarate <gabrielzzarate@gmail.com>
2019-10-30 16:08:02 -0400

    add Hurricane, Tidal, Market Value Impact

---
+++ src/api/tidal/tidal.spec.js
@@ -0,0 +1,37 @@
+const FirstStreet = require('../../__mocks__/FirstStreet');
+const tidalMock = require('../../__mocks__/tidalProperty');
+
+const validMethods = ['getPropertyByFSID', 'getCityByFSID', 'getPropertyByLatLng', 'getCityByLatLng', 'getPropertyByAddress', 'getCityByAddress'];
+
+describe('tidal', () => {
+  const fsf = new FirstStreet('aa.bb.cc', {
+    host: 'https://FAKE-HOST-FOR-TESTING.com',
+  });
+
+  it('should contain valid methods', () => {
+    validMethods.forEach((method) => {
+      expect(fsf.tidal[method]).toBeDefined();
+    });
+  });
+
+  it('.getPropertyByFSID should get property by ID', async () => {
+    const property = await fsf.tidal.getPropertyByFSID(tidalMock.FSID);
+
+    expect(property).toMatchSnapshot();
+    expect(property.results[0].floodType).toBe(tidalMock.results[0].floodType);
+  });
+
+  it('.getPropertyByLatLng should get property by lat, lng', async () => {
+    const property = await fsf.tidal.getPropertyByLatLng(-75.6451595133313, -75.6451595133313);
+
+    expect(property).toMatchSnapshot();
+    expect(property.results[0].floodType).toBe(tidalMock.results[0].floodType);
+  });
+
+  it('.getPropertyByAddress should get property by address lookup', async () => {
+    const property = await fsf.tidal.getPropertyByAddress('212 appoquin s, middletown, delware');
+
+    expect(property).toMatchSnapshot();
+    expect(property.results[0].floodType).toBe(tidalMock.results[0].floodType);
+  });
+});

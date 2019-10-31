05064a04e5751883cee8bf525ee06c49a323c5b9
gabrielzzarate <gabrielzzarate@gmail.com>
2019-10-31 10:28:24 -0400

    Rm floodType, add floodData

--- src/models/Response/index.js
+++ src/models/Response/index.js
@@ -4,7 +4,6 @@
  * Response Model
  * @typedef {Object} Response
  * @property {number} FSID - The location unique identifier
- * @property {string} type - The location type
  * @property {Object} results - Results of the query
 */
 class Response {
@@ -14,11 +13,6 @@ class Response {
 
   get FSID() {
     return this.data.FSID;
-  }
-
-  // type is the location type
-  get type() {
-    return this.data.type;
   }
 
   // results is an object of data that is associated with the data call
